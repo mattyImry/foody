e7e4e98a9bc95f675d17102bf1e5ca32525f4350
mattyImry <matteo.imeri@gmail.com>
2023-02-15 09:06:18 +0000

    set ingredients dynamically Ingredients.js comp

--- foody/src/components/Ingredients.js
+++ foody/src/components/Ingredients.js
@@ -1,12 +1,27 @@
+import React, { useState } from "react";
+
+import Button from "../UI/Button";
+
 const Ingredients = (props) => {
+  const [isClicked, setIsclicked] = useState(false);
+
+  function submitHandler() {
+    console.log("clicked");
+    setIsclicked((isClicked) => !isClicked);
+  }
   const ingredients = props.ingredientsText;
   return (
     <div>
-      <ol>
-        {ingredients.map((ingredient) => (
-          <li>{ingredient.text}</li>
-        ))}
-      </ol>
+      {isClicked && (
+        <ol>
+          {ingredients.map((ingredient) => (
+            <li>{ingredient.text}</li>
+          ))}
+        </ol>
+      )}
+      <Button className="button" onClick={submitHandler}>
+        {!isClicked ? "Show Ingredients" : "Hide Ingredients"}
+      </Button>
     </div>
   );
 };
