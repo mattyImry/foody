6490376eb640743ba8acb9967926408accec66b6
Matteo Imeri <matteo.imeri@gmail.com>
2023-02-22 09:43:43 +0000

    env file issue

--- foody/src/App.js
+++ foody/src/App.js
@@ -6,64 +6,65 @@ import Button from "./UI/Button";
 import Card from "./UI/Card";
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
-  const [search, setSearch] = useState("");
-  const [completeRequest, setCompleteRequest] = useState("");
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("");
+    const [completeRequest, setCompleteRequest] = useState("");
 
-  useEffect(() => {
-    const showRecipe = async () => {
-      const response = await fetch(
-        `https://api.edamam.com/api/recipes/v2?type=public&q=${completeRequest}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`
-      );
-      const data = await response.json();
-      setRecipes(data.hits);
+    useEffect(() => {
+        const showRecipe = async () => {
+            const response = await fetch(
+                `https://api.edamam.com/api/recipes/v2?type=public&q=${completeRequest}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`
+            );
+            const data = await response.json();
+            setRecipes(data.hits);
+        };
+        showRecipe();
+    }, [completeRequest]);
+
+    const inputHandler = (event) => {
+        setSearch(event.target.value);
     };
-    showRecipe();
-  }, [completeRequest]);
 
-  const inputHandler = (event) => {
-    setSearch(event.target.value);
-  };
+    const submitHandler = (event) => {
+        event.preventDefault();
+        setCompleteRequest(search);
+        setSearch("");
+    };
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    setCompleteRequest(search);
-    setSearch("");
-  };
+    return (
+        <div className={classes.foody}>
+            {!completeRequest ? <h1>What do you fancy today? </h1> : <h1>Enjoy!</h1>}
 
-  return (
-    <div className={classes.foody}>
-      {!completeRequest ? <h1>What do you fancy today? </h1> : <h1>Enjoy!</h1> }
-      
-      <form className={classes.search_form} onSubmit={submitHandler}>
-        <input
-          className={classes.search_bar}
-          type="text"
-          value={search}
-          onChange={inputHandler}
-          placeholder="Search Recipe"
-        />
-        <Button className="button" type="submit"><b>Discover</b></Button>
-      </form>
-      
-        <div className={classes.recipe_wrap}>
-          {recipes.map((recipe) => (
-            <Card className={classes.card_recipe}>
-              <Recipe
-                key={recipe.recipe.label}
-                title={recipe.recipe.label}
-                calories={recipe.recipe.calories}
-                image={recipe.recipe.image}
-                ingredients={recipe.recipe.ingredients}
-                type={recipe.recipe.mealType}
-                dishType={recipe.recipe.dishType}
-              />
-            </Card>
-          ))}
+            <form className={classes.search_form} onSubmit={submitHandler}>
+                <input
+                    className={classes.search_bar}
+                    type="text"
+                    value={search}
+                    onChange={inputHandler}
+                    placeholder="Search Recipe"
+                />
+                <Button className="button" type="submit">
+                    <b>Discover</b>
+                </Button>
+            </form>
+
+            <div className={classes.recipe_wrap}>
+                {recipes.map((recipe) => (
+                    <Card className={classes.card_recipe}>
+                        <Recipe
+                            key={recipe.recipe.label}
+                            title={recipe.recipe.label}
+                            calories={recipe.recipe.calories}
+                            image={recipe.recipe.image}
+                            ingredients={recipe.recipe.ingredients}
+                            type={recipe.recipe.mealType}
+                            dishType={recipe.recipe.dishType}
+                        />
+                    </Card>
+                ))}
+            </div>
         </div>
-      
-    </div>
-  );
+    );
 }
 
 export default App;
