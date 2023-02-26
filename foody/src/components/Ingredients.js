89540f9032bdc4a1053d1de487ba732d6e8d4e2f
Matteo Imeri <matteo.imeri@gmail.com>
2023-02-26 11:56:55 +0000

    closing ingredients by clicking ingredients

--- foody/src/components/Ingredients.js
+++ foody/src/components/Ingredients.js
@@ -4,27 +4,28 @@ import classes from "./Ingredients.module.css";
 import Button from "../UI/Button";
 
 const Ingredients = (props) => {
-  const [isClicked, setIsclicked] = useState(false);
-  const ingredients = props.ingredientsText;
+    const [isClicked, setIsclicked] = useState(false);
 
-  function submitHandler() {
-    setIsclicked((isClicked) => !isClicked);
-  }
+    const ingredients = props.ingredientsText;
 
-  return (
-    <div className={classes.ingredients_text}>
-      {isClicked && (
-        <ul>
-          {ingredients.map((ingredient) => (
-            <li>{ingredient.text}</li>
-          ))}
-        </ul>
-      )}
-      <Button className={classes.button} onClick={submitHandler}>
-        {!isClicked ? "Show Ingredients" : "Hide Ingredients"}
-      </Button>
-    </div>
-  );
+    const submitHandler = () => {
+        setIsclicked((isClicked) => !isClicked);
+    };
+
+    return (
+        <div className={classes.ingredients_text}>
+            {isClicked && (
+                <ul onClick={submitHandler}>
+                    {ingredients.map((ingredient) => (
+                        <li>{ingredient.text}</li>
+                    ))}
+                </ul>
+            )}
+            <Button className={classes.button} onClick={submitHandler}>
+                {!isClicked ? "Show Ingredients" : "Hide Ingredients"}
+            </Button>
+        </div>
+    );
 };
 
 export default Ingredients;
